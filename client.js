80c7c7e76abca3c5285076aab6de9f3151f95120
LeBonFire <155346153+LeBonFire@users.noreply.github.com>
2024-01-19 16:29:03 -0600

    Create client.js
    Attempting to figure out how to add JS to my Page

---
+++ client.js
@@ -0,0 +1,4 @@
+
+const myImage = new Image(200, 200);
+myImage.src = "https://mail.google.com/mail/u/0?ui=2&ik=741a57b574&attid=0.1&permmsgid=msg-f:1788469315996123000&th=18d1eb5dffa27f78&view=fimg&fur=ip&sz=s0-l75-ft&attbid=ANGjdJ9dq_LXbCEkgZg9MRd8F20OTNZqYKW8795quJ6DwrULDLMQDrTcB7TpPfQJdN88uOlg4co91KxgyTP32JQZaPfKq2BVPWHn9rl9Awc87w8cqmP5xHLrowjG8Wc&disp=emb&realattid=18d1eb5384275dc758e1";
+document.body.appendChild(myImage);
